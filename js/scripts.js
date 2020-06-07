62a5663791a9f12639a2fc6712a2c3b5267cd199
nestor.morales2@upr.edu <nestor.morales2@upr.edu>
2020-06-07 06:07:26 -0400

    new backend script

--- js/scripts.js
+++ js/scripts.js
@@ -165,6 +165,7 @@ $(document).ready(function () {
     $('.player').YTPlayer();
 
 
+
     /********************** Toggle Map Content **********************/
     $('#btn-show-map').click(function () {
         $('#map-content').toggleClass('toggle-map-content');
@@ -218,7 +219,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbwpYIOeI1YFnxBTvYmbBcxiWFXlvVPd6ffpXp2ry7hLTLxmUi_t/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbyT0ujyqTsbjvinNJXg2KSk4nBtVlKxIiHS-wYjHEz-8kxrBhs/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
